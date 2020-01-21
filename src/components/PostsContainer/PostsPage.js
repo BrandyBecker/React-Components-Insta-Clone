ca80c0b67fc2a03bc1cf29cb884b874a03ec0d5c
BrandyBecker <brandybeckerfsw@gmail.com>
2020-01-21 12:31:01 -0500

    Set up useState hook, imported DummyData and SearchBarContainer, set up State for the Data, added SearchBar and PostsContainer components to PostsPage

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,13 +1,23 @@
 // You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 // import the dummy-data and the SearchBar
 import PostsContainer from "./PostsContainer";
+import SearchBarContainer from '../SearchBar/SearchBarContainer.js'
+import dummydata from '../dummy-data.js'
 
 const PostsPage = () => {
   // set up state for the data
-
+    const [postData, setPostData] = useState(dummydata);
+    console.log("initial state", postData);
   // add the SearchBar and PostsContainer components here
-  return <div className="App" />;
+ 
+  return (
+    <div className="App">
+      <SearchBarContainer />
+      <PostsContainer postData={postData}/>
+
+
+    </div>);
 };
 
 export default PostsPage;
